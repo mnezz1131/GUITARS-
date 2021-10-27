66ba1050986f8f5db95b5ec0a578d6163c77e5aa
Mike <mnezz1131@gmail.com>
2021-10-27 13:14:36 -0500

    fixed air table link

--- guitar-wars/src/components/pages/GuitarPage/GuitarPage.js
+++ guitar-wars/src/components/pages/GuitarPage/GuitarPage.js
@@ -8,15 +8,20 @@ console.log(id)
   const gtrPlayer = gtrPlayers.find((element)=> element.id === id)
   
   return (
-    <div>
-   
-      <h1>This is my Guitarist Page</h1>
+    <div className="gtrPage">
+      <div className="rowgtr" >
+      <div className="column">
+      <h1>Guitarist Page</h1>
       <h3>Name:{gtrPlayer?.fields.names }</h3>
        <h3>Rank:{gtrPlayer?.fields.rank }</h3>
       <h3>Genre:{gtrPlayer?.fields.genre }</h3>
-      <h3>Bands:{gtrPlayer?.fields.bands }</h3>
-      <img alt={gtrPlayer?.fields.names } src ={gtrPlayer?.fields.large }/>
-    
+          <h3>Bands:{gtrPlayer?.fields.bands}</h3>
+        </div>
+        
+        <div className="column">
+          <img alt={gtrPlayer?.fields.names} src={gtrPlayer?.fields.large} />
+          </div>
+      </div>
     </div>
   )
   
